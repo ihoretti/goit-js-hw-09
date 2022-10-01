bc79d76e021268bf5a997b0eefd878b90479b9f2
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-10-01 22:53:13 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,6 +9,32 @@
 
 //Для генерации случайного цвета используй функцию getRandomHexColor.
 
+const refs = {
+  getStartBtn: document.querySelector('[data-start]'),
+  getStopBtn: document.querySelector('[data-stop]'),
+  getBody: document.querySelector('body'),
+};
+
+refs.getStartBtn.addEventListener('click', clickStart);
+refs.getStopBtn.addEventListener('click', clickStop);
+
+refs.getStopBtn.disabled = true;
+let intervalId = 0;
+
+function clickStart() {
+  refs.getStartBtn.disabled = true;
+  refs.getStopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    refs.getBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function clickStop() {
+  clearTimeout(intervalId);
+  refs.getStartBtn.disabled = false;
+  refs.getStopBtn.disabled = true;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
